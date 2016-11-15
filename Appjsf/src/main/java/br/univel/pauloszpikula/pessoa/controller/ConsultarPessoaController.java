410941a94999cac4c33acb743f466b5a901f0ac9
paulo <paulo.szpikula10@hotmail.com>
2016-11-14 22:21:06 -0200

    Parte 7 - Criação da Query de Consulta; Criação fo método de consulta GetPessoas; Criação do Controller para a consulta; Criação da página de consulta.

---
+++ Appjsf/src/main/java/br/univel/pauloszpikula/pessoa/controller/ConsultarPessoaController.java
@@ -0,0 +1,71 @@
+package br.univel.pauloszpikula.pessoa.controller;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Produces;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import br.univel.pauloszpikula.model.PessoaModel;
+import br.univel.pauloszpikula.repository.PessoaRepository;
+
+/**
+ * Controlador de consulta de pessoas.
+ * @author Paulo Szpikula 14/11/2016 22:10
+ */
+@Named(value="consultarPessoaController")
+@ViewScoped
+public class ConsultarPessoaController implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject transient
+	private PessoaModel pessoaModel;
+
+	@Produces
+	private List<PessoaModel> pessoas;
+
+	@Inject transient
+	private PessoaRepository pessoaRepository;
+
+	/**
+	 * Pega o valor da variável pessoas.
+	 * @return uma List especificando a lista de pessoas.
+	 */
+	public List<PessoaModel> getPessoas() {
+		return pessoas;
+	}
+
+	/**
+	 * Atribui valor a variável pessoas.
+	 * @param pessoas uma lista de pessoas.
+	 */
+	public void setPessoas(List<PessoaModel> pessoas) {
+		this.pessoas = pessoas;
+	}
+
+	/**
+	 * Pega o valor da variável pessoaModel.
+	 * @return uma PessoaModel especificando a pessoa.
+	 */
+	public PessoaModel getPessoaModel() {
+		return pessoaModel;
+	}
+
+	/**
+	 * Atribui valor a variável pessoaModel.
+	 * @param pessoaModel a pessoa.
+	 */
+	public void setPessoaModel(PessoaModel pessoaModel) {
+		this.pessoaModel = pessoaModel;
+	}
+
+	/***
+	 * Carrega as pessoas na inicialização.
+	 */
+	@PostConstruct
+	public void init(){
+		this.pessoas = pessoaRepository.GetPessoas(); // Retorna as pessoas cadastradas
+	}
+}
--- Appjsf/src/main/java/br/univel/pauloszpikula/repository/PessoaRepository.java
+++ Appjsf/src/main/java/br/univel/pauloszpikula/repository/PessoaRepository.java
@@ -1,9 +1,14 @@
 package br.univel.pauloszpikula.repository;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import br.univel.pauloszpikula.model.PessoaModel;
+import br.univel.pauloszpikula.model.UsuarioModel;
 import br.univel.pauloszpikula.repository.entity.PessoaEntity;
 import br.univel.pauloszpikula.repository.entity.UsuarioEntity;
 import br.univel.pauloszpikula.uteis.Uteis;
@@ -35,5 +40,41 @@ public class PessoaRepository {
 		UsuarioEntity usuarioEntity = entityManager.find(UsuarioEntity.class, pessoaModel.getUsuarioModel().getCodigo());
 		pessoaEntity.setUsuarioEntity(usuarioEntity);
 		entityManager.persist(pessoaEntity);
+	}
+
+
+	/***
+	 * Responsável por consultar as pessoas do banco de dados.
+	 * @return uma List de pessoas.
+	 */
+	public List<PessoaModel> GetPessoas(){
+		List<PessoaModel> pessoasModel = new ArrayList<PessoaModel>();
+		entityManager =  Uteis.JpaEntityManager();
+		Query query = entityManager.createNamedQuery("PessoaEntity.findAll");
+		@SuppressWarnings("unchecked")
+		Collection<PessoaEntity> pessoasEntity = (Collection<PessoaEntity>)query.getResultList();
+		PessoaModel pessoaModel = null;
+		for (PessoaEntity pessoaEntity : pessoasEntity) {
+			pessoaModel = new PessoaModel();
+			pessoaModel.setCodigo(pessoaEntity.getCodigo());
+			pessoaModel.setDataCadastro(pessoaEntity.getDataCadastro());
+			pessoaModel.setEmail(pessoaEntity.getEmail());
+			pessoaModel.setEndereco(pessoaEntity.getEndereco());
+			pessoaModel.setNome(pessoaEntity.getNome());
+			if (pessoaEntity.getOrigemCadastro().equals("X"))
+				pessoaModel.setOrigemCadastro("XML");
+			else
+				pessoaModel.setOrigemCadastro("INPUT");
+			if (pessoaEntity.getSexo().equals("M"))
+				pessoaModel.setSexo("Masculino");
+			else
+				pessoaModel.setSexo("Feminino");
+			UsuarioEntity usuarioEntity =  pessoaEntity.getUsuarioEntity();
+			UsuarioModel usuarioModel = new UsuarioModel();
+			usuarioModel.setUsuario(usuarioEntity.getUsuario());
+			pessoaModel.setUsuarioModel(usuarioModel);
+			pessoasModel.add(pessoaModel);
+		}
+		return pessoasModel;
 	}
 }
